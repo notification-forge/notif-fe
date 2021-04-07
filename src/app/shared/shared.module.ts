c366e2532248eae2c92d15731b7019a9b2e3da04
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-04-07 15:40:48 +0800

    made side nav

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SideNavComponent],
-  imports: [CommonModule, NzMenuModule, NzIconModule],
+  imports: [CommonModule, NzMenuModule, NzIconModule, RouterModule],
   exports: [SideNavComponent],
 })
 export class SharedModule {}
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-side-nav',
@@ -7,12 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
   isCollapsed = false;
+  sideNavItem: SideNavItem[] = [
+    {
+      url: 'template',
+      display: 'Templates',
+      icon: 'home',
+    },
+    {
+      url: 'image-lib',
+      display: 'Image Library',
+      icon: 'home',
+    },
+  ];
 
-  constructor() {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationStart))
+      .subscribe((event) => {
+        console.log(event);
+      });
+  }
 
   toggleCollapsed(): void {
     this.isCollapsed = !this.isCollapsed;
   }
+}
+
+interface SideNavItem {
+  url: string;
+  display: string;
+  icon: string;
 }
