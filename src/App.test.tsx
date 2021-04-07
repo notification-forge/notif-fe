38e224b9535f2d4118f37dbeaac2262508f72463
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-04-07 14:01:43 +0800

    updated smoke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  it('renders correctly', () => {
+    const AppComponent = shallow(<App />);
+    expect(AppComponent).toBeTruthy();
+  });
 });
