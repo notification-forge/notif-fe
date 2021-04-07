ffb5b52b65233db31733d1b684d703551ea161b4
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-04-07 14:23:16 +0800

    remove to change to angular

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import App from './App';
-
-describe('App Component', () => {
-  it('renders correctly', () => {
-    const AppComponent = shallow(<App />);
-    expect(AppComponent).toBeTruthy();
-  });
-});
--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import './App.scss';
-
-function App() {
-  return <div className="app-wrapper">App</div>;
-}
-
-export default App;
