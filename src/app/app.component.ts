d83adc22a10cc6184e25041215d930ed237ff8ee
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-04-19 11:35:30 +0800

    added user is logged in check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  userIsLoggedIn: boolean = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.user$.subscribe((user) => {
+      this.userIsLoggedIn = !!user;
+    });
+  }
+}
