289ea7ee8f5f1d17d876563535d2306f5ccf36eb
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-04-19 11:25:51 +0800

    added placeholder login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -13,6 +13,10 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GraphQLModule } from './graphql/graphql.module';
 import { LoginComponent } from './login/login.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 registerLocaleData(en);
 
@@ -28,6 +32,11 @@ registerLocaleData(en);
     NzMenuModule,
     SharedModule,
     GraphQLModule,
+    NzFormModule,
+    NzButtonModule,
+    NzIconModule,
+    NzInputModule,
+    ReactiveFormsModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  loginForm: FormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  handleFormSubmit(): void {
+    console.log(this.loginForm.value);
+  }
 }
