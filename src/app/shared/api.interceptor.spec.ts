8b204e827d86e08e76e76060bb6623a87e6c20f5
deepna <deepnathalanki123@gmail.com>
2021-04-23 08:46:46 +0530

    added RouterTestingModule,HttpClientTestingModule imports

--- src/app/shared/api.interceptor.spec.ts
+++ src/app/shared/api.interceptor.spec.ts
@@ -2,9 +2,13 @@ import { TestBed } from '@angular/core/testing';
 
 import { ApiInterceptor } from './api.interceptor';
 
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('ApiInterceptor', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
       providers: [ApiInterceptor],
     })
   );
