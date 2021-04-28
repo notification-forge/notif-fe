43a66733ff07ed960da5c0e6766bea56d6f1d7bb
deepna <deepnathalanki123@gmail.com>
2021-04-28 17:59:32 +0530

    imported NzMessageService, OverlayModule

--- src/app/shared/auth.guard.spec.ts
+++ src/app/shared/auth.guard.spec.ts
@@ -1,7 +1,9 @@
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { OverlayModule } from '@angular/cdk/overlay';
 import { AuthGuard } from './auth.guard';
 
 describe('AuthGuard', () => {
@@ -9,7 +11,8 @@ describe('AuthGuard', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientModule],
+      imports: [RouterTestingModule, HttpClientModule, OverlayModule],
+      providers: [NzMessageService],
     });
     guard = TestBed.inject(AuthGuard);
   });
