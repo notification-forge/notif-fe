1afebba80ade69535675638132f8b53eb9637896
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-04-29 14:51:36 +0800

    added placeholder saving

--- src/app/templates/editor.service.ts
+++ src/app/templates/editor.service.ts
@@ -6,26 +6,36 @@ import { TemplateStatus } from '../graphql/graphql';
   providedIn: 'root',
 })
 export class EditorService {
+  private _designCodeBody: string | null;
+  private _templateVersionId: number;
   settingsForm: FormGroup = this.fb.group({
     from: ['', Validators.required],
     to: ['', Validators.required],
     cc: [''],
     bcc: [''],
   });
-  private _designCodeBody: string | null;
   templateVersionName: string;
   status: TemplateStatus;
 
   constructor(private fb: FormBuilder) {}
 
   initializeEmail(
+    templateVersionId: number,
     designCodeBody: string | null,
     templateVersionName: string,
     status: TemplateStatus
   ) {
+    this._templateVersionId = templateVersionId;
     this._designCodeBody = designCodeBody;
     this.templateVersionName = templateVersionName;
     this.status = status;
+  }
+
+  saveTemplate() {
+    console.log(
+      'saving to backend with template version id',
+      this._templateVersionId
+    );
   }
 
   get designCodeBody() {
@@ -34,5 +44,6 @@ export class EditorService {
 
   set designCodeBody(newBody: string | null) {
     this._designCodeBody = newBody;
+    this.saveTemplate();
   }
 }
