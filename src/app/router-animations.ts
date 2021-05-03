9b3ee0759b077b246b8866e7eb718ed4a02ae66d
Le Thanh An <lethanhan@Les-MacBook-Pro.local>
2021-05-03 13:46:31 +0800

    added slide up animation for code editor

--- src/app/router-animations.ts
+++ src/app/router-animations.ts
@@ -7,6 +7,37 @@ import {
 } from '@angular/animations';
 
 export const fade = trigger('routeAnimations', [
+  transition('* => templates-editor', [
+    query(
+      ':enter',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          left: 0,
+          transform: 'translateY(200%)',
+          opacity: 1,
+          zIndex: 1000,
+        }),
+      ],
+      { optional: true }
+    ),
+
+    query(
+      ':enter',
+      [
+        animate(
+          '200ms ease',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        ),
+      ],
+      {
+        optional: true,
+      }
+    ),
+  ]),
+
   transition('* <=> *', [
     query(
       ':enter',
